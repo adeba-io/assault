98cc6f9d8bad46b2b1d88dd82cad8fa0ea831867
TiGBS <adebaOT@outlook.com>
2018-08-09 22:55:50 +0100

    Second Commit
    Signed-off-by: TiGBS <adebaOT@outlook.com>

---
+++ Assets/Scripts/Editor/PlatformEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Platform))]
+public class PlatformEditor : Editor
+{
+    Platform platform;
+
+    SerializedProperty _slopeAngle;
+    SerializedProperty _physicsMaterial;
+
+    readonly GUIContent gui_slopeAngle = new GUIContent("Slope Angle", "The rotation of the attached transform");
+    readonly GUIContent gui_physicsMaterial = new GUIContent("Physics Material");
+
+    private void OnEnable()
+    {
+        platform = (Platform)target;
+
+        _slopeAngle = serializedObject.FindProperty("_slopeAngle");
+        _physicsMaterial = serializedObject.FindProperty("_physicsMaterial");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.Slider(_slopeAngle, -90f, 90f, gui_slopeAngle);
+        platform.transform.rotation = Quaternion.Euler(0, 0, _slopeAngle.floatValue);
+
+        EditorGUILayout.PropertyField(_physicsMaterial, gui_physicsMaterial);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Platform : MonoBehaviour
+{
+    [Serializable]
+    public class CaughtObject
+    {
+        public Rigidbody2D rigidbody;
+        public Collider2D collider;
+        public PhysicsObject physicsObject;
+
+        public bool inContact;
+        public bool checkedThisFrame;
+    }
+
+    [SerializeField] PhysicsMaterial2D _physicsMaterial;
+    [SerializeField] [Range(-90f, 90f)] float _slopeAngle;
+
+    Rigidbody2D _rigidbody2D;
+
+    public float slopeAngle { get { return _slopeAngle; } }
+    public float friction { get { return _physicsMaterial.friction; } }
+    public float bounciness { get { return _physicsMaterial.bounciness; } }
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+}
